8a1505bc7559be409c4ed9d1722026d36732050f
azgmohammadd <azgmohammadd@gmail.com>
2023-02-02 20:05:13 +0330

    fix bugs in create models

--- main.py
+++ main.py
@@ -12,27 +12,24 @@ def main():
     # create list of nodes
     numberOfHalls, numberOfGroups = tuple(map(int, lines[0].split()))
     
-    halls: list[Hall] = [Hall(f'Hall{index}') for index in range(numberOfHalls)]
-    groups: list[Group] = [Group(f'Group{index}') for index in range(numberOfGroups)]
+    # create list of halls and groups
+    halls: list[Hall] = [Hall(f'Hall{index + 1}') for index in range(numberOfHalls)]
+    groups: list[Group] = [Group(f'Group{index + 1}') for index in range(numberOfGroups)]
     
-    
+    # add prefrences to halls
     for index in range(1, numberOfGroups + 1):
         prefences = lines[index].split()
         
-            
         for preference in prefences:
             halls[int(preference) - 1].addPrefrence(groups[index - 1])
             
-            print(f'hall:{preference}, group:{index}') 
-            
+    # add nighbors to halls
+    for index in range(numberOfGroups + 2, len(lines)):
+        hall_i, hall_j = lines[index].split()
         
-    # for index in range(numberOfGroups + 2, len(lines)):
-    #     hall_i, hall_j = lines[index].split()
-        
-    #     hall = halls[int(hall_i) - 1]
-    #     hall.addNighbor(halls[int(hall_j) - 1])
+        hall = halls[int(hall_i) - 1]
+        hall.addNighbor(halls[int(hall_j) - 1])
             
-    halls
             
 if __name__ == '__main__':
     main()
