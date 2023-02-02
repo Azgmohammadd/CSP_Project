ad9495c4dd2df22c2c166dc6a26898152bc1fc44
azgmohammadd <azgmohammadd@gmail.com>
2023-02-02 16:09:29 +0330

    read from input and create model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from node import Node
+from Models.group import Group
+from Models.hall import Hall
 
 def main():
     # read text file
@@ -9,8 +10,29 @@ def main():
     lines = lines.splitlines()
 
     # create list of nodes
-    numberOfGroup, numberOfRoom = lines[0].split(" ")
-
+    numberOfHalls, numberOfGroups = tuple(map(int, lines[0].split()))
     
+    halls: list[Hall] = [Hall(f'Hall{index}') for index in range(numberOfHalls)]
+    groups: list[Group] = [Group(f'Group{index}') for index in range(numberOfGroups)]
+    
+    
+    for index in range(1, numberOfGroups + 1):
+        prefences = lines[index].split()
+        
+            
+        for preference in prefences:
+            halls[int(preference) - 1].addPrefrence(groups[index - 1])
+            
+            print(f'hall:{preference}, group:{index}') 
+            
+        
+    # for index in range(numberOfGroups + 2, len(lines)):
+    #     hall_i, hall_j = lines[index].split()
+        
+    #     hall = halls[int(hall_i) - 1]
+    #     hall.addNighbor(halls[int(hall_j) - 1])
+            
+    halls
+            
 if __name__ == '__main__':
     main()
