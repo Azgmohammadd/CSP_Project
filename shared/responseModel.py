b727b12b26e993f34caf487ed995db18aa7281f1
azgmohammadd <azgmohammadd@gmail.com>
2023-02-03 13:37:56 +0330

    update responseModel

--- shared/responseModel.py
+++ shared/responseModel.py
@@ -1,15 +1,15 @@
 from typing import Any
 
 class ResponseModel():
-    __result: Any
-    __hasError: bool
-    __message: str
+    result: Any
+    hasError: bool
+    message: str
     
     def __init__(self, response: Any, hasError: bool, message: str):
-        self.__result = response
-        self.__hasError = hasError
-        self.__message = message
+        self.result = response
+        self.hasError = hasError
+        self.message = message
         
     
     def __str__(self) -> str:
-        return f"result: {self.__result}, hasError: {self.__hasError}, message: {self.__message}"
+        return f"result: {self.result}, hasError: {self.hasError}, message: {self.message}"
