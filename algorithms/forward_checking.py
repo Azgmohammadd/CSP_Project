1975fcbdd3318d00d1fd339852949e0fbe8acdf4
azgmohammadd <azgmohammadd@gmail.com>
2023-02-04 02:56:07 +0330

    update FC (add optioanl parameter MRV and LCV  )

--- algorithms/forward_checking.py
+++ algorithms/forward_checking.py
@@ -7,21 +7,20 @@ from copy import deepcopy
 
 
 #forward checking algorithm
-def forwardChecking(halls: list[Hall], index: int = 0) -> ResponseModel:
+def forwardChecking(halls: list[Hall], index: int = 0, MRV = None, LCV = None) -> ResponseModel:
     """forward checking algorithm"""
     
-    hall = MRV(halls)
-
+    hall = MRV(halls) if MRV is not None else halls[index % len(halls)]
+    
     index = halls.index(hall)
     
     # check if hall has prefrences to assign value
     if (len(hall.getPrefrences()) == 0):
         return ResponseModel([], True, 'No prefrences')
     
-    # sort the preferences based on LCV heuristic
-    # sortedPrefrences = LCV(hall, halls)
-
-    for prefrence in LCV(hall):
+    prefrences = LCV(hall) if LCV is not None else hall.getPrefrences()
+    
+    for prefrence in prefrences:
         copy_halls = deepcopy(halls)
         hall = copy_halls[index]
         
@@ -36,9 +35,9 @@ def forwardChecking(halls: list[Hall], index: int = 0) -> ResponseModel:
             return ResponseModel(copy_halls, False, 'forward checking completed')
         
         # check if forward checking is completed successfully in the next halls  
-        if (not forwardChecking(copy_halls, halls.index(MRV(halls))).hasError):
-            next_index = halls.index(MRV(halls))
-            
-            copy_halls = forwardChecking(copy_halls, next_index).result 
+        forward = forwardChecking(halls = copy_halls, index = halls.index(MRV(halls)), MRV= MRV, LCV= LCV) if MRV is not None else forwardChecking(halls = copy_halls, index= index + 1)
+                
+        if (not forward.hasError):
+            copy_halls = forward.result 
             
             return ResponseModel(copy_halls, False, 'forward checking completed')
