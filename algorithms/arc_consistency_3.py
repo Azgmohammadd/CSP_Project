b1c742c0759d27809cacfad3f600bab5ee019ec1
azgmohammadd <azgmohammadd@gmail.com>
2023-02-04 16:05:39 +0330

    fix AC3 algorithm

--- algorithms/arc_consistency_3.py
+++ algorithms/arc_consistency_3.py
@@ -1,31 +1,32 @@
-from copy import deepcopy
 from shared.responseModel import ResponseModel
 from Models.hall import Hall
 
+
 def AC3(halls: list[Hall]) -> ResponseModel:
     """AC3 algorithm"""
-    queue = [(hall, nighbor) for hall in halls for nighbor in hall.getNighbors()]
+    queue: list[tuple(Hall, Hall)] = [(hall, neighbor) for hall in halls for neighbor in hall.getNeighbors()]
     
     while queue:
-        hall, nighbor = queue.pop(0)
-        if revise(hall, nighbor):
-            if len(hall.getPefrences()) == 0:
+        hall, neighbor = queue.pop(0)
+        
+        if revise(hall, neighbor):
+            if len(hall.getPrefrences()) == 0:
                 return ResponseModel([], True, 'Contradiction detected in AC3')
             
-            for n in hall.getNighbors():
-                if n != nighbor:
+            for n in hall.getNeighbors():
+                if n != neighbor:
                     queue.append((n, hall))
-                    
+                     
     return ResponseModel(halls, False, 'AC3 completed successfully')
 
 
-def revise(hall: Hall, nighbor: Hall) -> bool:
-    """check if hall and nighbor have a common value in their preferences, 
+def revise(hall: Hall, neighbor: Hall) -> bool:
+    """check if hall and neighbor have a common value in their preferences, 
     if not remove the value from hall's preferences"""
     revised = False
     
-    for value in hall.getPefrences():
-        if value not in nighbor.getPefrences():
+    for value in hall.getPrefrences():
+        if not any(prefrence.getName() != value.getName() for prefrence in neighbor.getPrefrences()):
             hall.removePrefrence(value)
             revised = True
             
