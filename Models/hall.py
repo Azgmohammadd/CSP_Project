de2b45f82b99fb3367f4c54b099a02ed5ff3c4df
azgmohammadd <azgmohammadd@gmail.com>
2023-02-17 23:20:53 +0330

    update hall

--- Models/hall.py
+++ Models/hall.py
@@ -1,18 +1,17 @@
 from Models.group import Group
-from shared.responseModel import ResponseModel
 
 
 class Hall():
     __name: str
     __value: Group
     __neighbors: set['Hall']
-    __prefrences: set[Group] 
+    __preferences: set[Group] 
     
     def __init__(self, name: str):
         self.__name = name
         self.__value = None
         self.__neighbors = set()
-        self.__prefrences = set()
+        self.__preferences = set()
     
     
     def getName(self) -> str:
@@ -25,10 +24,6 @@ class Hall():
 
     def getValue(self) -> Group:
         return self.__value
-    
-    
-    def updateNeighbors(self, neighbors: set['Hall']):
-        self.__neighbors = neighbors
         
     
     def addNeighbor(self, neighbor: 'Hall'):
@@ -39,28 +34,16 @@ class Hall():
         return self.__neighbors
     
     
-    def addPrefrence(self, prefrence: Group):
-        self.__prefrences.add(prefrence)
+    def addPreference(self, preference: Group):
+        self.__preferences.add(preference)
         
     
-    def addPrefrences(self, prefrences: set[Group]):
-        self.__prefrences = prefrences
+    def removePreference(self, preference: Group): # -> ResponseModel:
+        self.__preferences = set(filter(lambda x: x.getName() != preference.getName(), self.__preferences))
     
     
-    def isExistPrefrence(self, prefrence: Group) -> bool:
-        for perf in self.__prefrences:
-            if (perf.getName() == prefrence.getName()):
-                return True
-
-        return False
-    
-    
-    def removePrefrence(self, prefrence: Group): # -> ResponseModel:
-        self.__prefrences = set(filter(lambda x: x.getName() != prefrence.getName(), self.__prefrences))
-    
-    
-    def getPrefrences(self) -> set[Group]:
-        return self.__prefrences
+    def getPreferences(self) -> set[Group]:
+        return self.__preferences
     
     
     def checkAll(halls : list['Hall']) -> bool:
